99ff738ab663f9944a8f29be8369caa2cfa8b163
Vinicius <viniciusnot106@gmail.com>
2023-11-06 08:01:33 -0300

    Add crypt and decrypt

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ const schemaValidator = require('../src/apps/middlewares/schemaValidator');
 
 const userSchema = require('../src/schema/create.user.schema.json')
 const UserController = require('./apps/controllers/UserController');
+const AuthenticationController = require('./apps/controllers/AuthenticationController');
+const authSchema = require('../src/schema/auth.schema.json');
 
 const routes = new Router();
 
 routes.post('/user', schemaValidator(userSchema), UserController.create);
+
+routes.post('/auth', schemaValidator(authSchema), AuthenticationController.authenticate);
 
 routes.get('/health', (req, res) => res.send({
   message: 'Connected with success in port 3000',
